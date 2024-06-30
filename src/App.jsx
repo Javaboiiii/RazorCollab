04bd2bc86a822c23bf7285ef8d1389de4c61b085
Javaboii <139529420+Javaboiiii@users.noreply.github.com>
2024-06-30 10:14:53 +0530

    created working unstyled editor page, where we can join & collab in real time

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from 'react'
+import {
+  useLocation,
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+  Navigate,
+  useParams,
+} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast'
 import './App.css'
-import Editor from './components/Editor/Editor'
+import EditorPage from './EditorPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <></>,
+    },
+    {
+      path: "/editor/:roomId",
+      element: <EditorPage />
+    }
+  ]);
 
   return (
     <>
-      <Editor/>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          success: {
+            theme: {
+              primary: '#4aed88',
+            },
+          },
+        }}
+      ></Toaster>
+      <RouterProvider router={router} />
     </>
   )
 }
